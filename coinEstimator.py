733a3ce9d988495f39ca05910fefdac084524e4b
Adam57055 <adamtwam85@gmail.com>
2026-03-17 10:34:59 -0400

    add mathematical calculations to continue program and find amount of coins in each wrapper

--- coinEstimator.py
+++ coinEstimator.py
@@ -9,7 +9,7 @@ pennyWrapper = 50
 nickelWrapper = 40
 dimeWrapper = 50
 quarterWrapper = 40
-
+#weight is estimated by amount of coins in a wrapper
 print("Welcome to the Coin Estimator!")
 print("Please enter the weight of your: ")
 
@@ -17,3 +17,15 @@ pennies = float(input("Pennies: "))
 nickels = float(input("Nickels:" ))
 dimes = float(input("Dimes: "))
 quarters = float(input("Quarters: "))
+
+#amount of coins by the information user inputs
+penny_amount = pennies / pennyWeight
+nickel_amount = nickels / nickelWeight
+dime_amount = dimes / dimeWeight
+quarter_amount = quarters / quarterWeight
+
+#Print results
+print(penny_amount)
+print(nickel_amount)
+print(dime_amount)
+print(quarter_amount)
